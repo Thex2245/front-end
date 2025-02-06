b782ae3b19fb14528702990eaebe54d1cb37557d
Thex <thexduvoley@gmail.com>
2025-02-06 14:00:49 -0300

    Adiciona metadados para a página inicial do dashboard de usuários

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Metadata } from "next";
 import TableComponent from "./components/table";
 
+export const metadata: Metadata = {
+  title: 'Dashboard Users',
+  description: 'Manuseie o dashboard de usuários da API',
+}
+
 export default function Home() {
   return (
     <div className="p-10 grid gap-4">
